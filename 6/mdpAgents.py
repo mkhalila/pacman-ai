7a811224a26a738917679477a5a97d46d2acc688
Khalil Ahmed <mkhalila@outlook.com>
2019-11-30 17:48:24 +0000

    6: Remove grid class and map

--- 6/mdpAgents.py
+++ 6/mdpAgents.py
@@ -34,77 +34,6 @@ import game
 import util
 import sys
 
-#
-# A class that creates a grid that can be used as a map
-#
-# The map itself is implemented as a nested list, and the interface
-# allows it to be accessed by specifying x, y locations.
-#
-class Grid:
-         
-    # Constructor
-    #
-    # Note that it creates variables:
-    #
-    # grid:   an array that has one position for each element in the grid.
-    # width:  the width of the grid
-    # height: the height of the grid
-    #
-    # Grid elements are not restricted, so you can place whatever you
-    # like at each location. You just have to be careful how you
-    # handle the elements when you use them.
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        subgrid = []
-        for i in range(self.height):
-            row=[]
-            for j in range(self.width):
-                row.append(0)
-            subgrid.append(row)
-
-        self.grid = subgrid
-
-    # Print the grid out.
-    def display(self):       
-        for i in range(self.height):
-            for j in range(self.width):
-                # print grid elements with no newline
-                print self.grid[i][j],
-            # A new line after each line of the grid
-            print 
-        # A line after the grid
-        print
-
-    # The display function prints the grid out upside down. This
-    # prints the grid out so that it matches the view we see when we
-    # look at Pacman.
-    def prettyDisplay(self):       
-        for i in range(self.height):
-            for j in range(self.width):
-                # print grid elements with no newline
-                print self.grid[self.height - (i + 1)][j],
-            # A new line after each line of the grid
-            print 
-        # A line after the grid
-        print
-        
-    # Set and get the values of specific elements in the grid.
-    # Here x and y are indices.
-    def setValue(self, x, y, value):
-        self.grid[y][x] = value
-
-    def getValue(self, x, y):
-        return self.grid[y][x]
-
-    # Return width and height to support functions that manipulate the
-    # values stored in the grid.
-    def getHeight(self):
-        return self.height
-
-    def getWidth(self):
-        return self.width
-
 class MDPAgent(Agent):
 
     def __init__(self):
@@ -112,11 +41,11 @@ class MDPAgent(Agent):
 
     def registerInitialState(self, state):
         # print "Running registerInitialState!"
-        self.map = self.makeMap(state)
         self.walls = api.walls(state)
         self.nonWalls = []
-        for i in range(self.map.getWidth()):
-            for j in range(self.map.getHeight()):
+        (x, y) = self.calcLayoutBounds(state)
+        for i in range(x):
+            for j in range(y):
                 if (i, j) not in self.walls:
                     self.nonWalls.append((i, j))
         self.values = dict.fromkeys(self.nonWalls, 0)
@@ -136,11 +65,11 @@ class MDPAgent(Agent):
     def final(self, state):
         print "Looks like I just died!"
 
-    def makeMap(self, state):
+    def calcLayoutBounds(self, state):
         corners = api.corners(state)
         height = self.getLayoutHeight(corners)
         width  = self.getLayoutWidth(corners)
-        return Grid(width, height)
+        return (width, height)
 
     def getLayoutHeight(self, corners):
         height = -1
